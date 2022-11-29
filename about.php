ae6c303ac16220d65bb9af7445c35e4c055d132a
unknown <phamduyenn2906@gmail.com>
2022-11-29 14:55:16 +0700

    Update about.php

--- about.php
+++ about.php
@@ -1,19 +1,32 @@
  <!-- Masthead-->
-        <header >
+        <header style="margin-top: 100px;">
             <div class="container h-100">
                 <div class="row h-100 align-items-center justify-content-center text-center">
-                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
-                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                    <div class="col-lg-10 align-self-end">
+                    	 <h1 class="text-uppercase font-weight-bold" style="padding: 25px 0;">About Us</h1>
                         <hr class="divider my-4" />
                     </div>
                     
                 </div>
             </div>
-        </header>
 
+            <img src="assets/img/logo-doc.png" alt="" class="logo_doc">
+            
+        </header>
     <section class="page-section">
         <div class="container">
     <?php echo html_entity_decode($_SESSION['setting_about_content']) ?>        
             
         </div>
         </section>
+
+        <style>
+            .logo_doc {
+                display: block; 
+                height: 300px;
+                margin-left: auto; 
+                margin-right: auto; 
+                margin-top: 128px;
+                padding: 30px;
+            }
+        </style>
